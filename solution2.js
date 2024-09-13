c12b017989cc8cfe97e2b94d78a61d8d6006702a
jitender1222 <jeetusinghkanyal@gmail.com>
2024-09-13 13:25:27 +0530

    clock using setTimeout

--- solution2.js
+++ solution2.js
@@ -1,6 +1,26 @@
 
 // clock using set Interval
 
+
+// const updateClock = () => {
+//     const now = new Date();
+
+//     const hour24= now.getHours();
+//     const minute24=now.getMinutes();
+//     const seconds24=now.getSeconds();
+
+//     const hour12= ((now.getHours() + 11 ) % 12 ) +1 
+//     const ampm= now.getHours() >=12 ? "pm" : "am" 
+
+//     console.log(`24Hours Format : ${hour24}:${minute24}:${seconds24}${ampm}`)
+//     console.log(`12Hours Format : ${hour12}:${minute24}:${seconds24}${ampm}`)
+// };
+
+// setInterval(updateClock,1000)
+
+
+// clock using setTimeout
+
 const updateClock = () => {
     const now = new Date();
 
@@ -13,6 +33,9 @@ const updateClock = () => {
 
     console.log(`24Hours Format : ${hour24}:${minute24}:${seconds24}${ampm}`)
     console.log(`12Hours Format : ${hour12}:${minute24}:${seconds24}${ampm}`)
+
+    setTimeout(updateClock,1000);
 };
 
-setInterval(updateClock,1000)
+updateClock();
+
