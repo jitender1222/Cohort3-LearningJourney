7bac5a772386ea92eb6dc946c68b054dc0209df0
jitender1222 <jeetusinghkanyal@gmail.com>
2024-09-16 16:55:48 +0530

    backgrounde color changer

---
+++ week-3/bgChanger/bgChanger.js
@@ -0,0 +1,31 @@
+
+function chnageColor(input){
+
+    if(input == red){
+        document.body.style.backgroundColor = "red";
+        input.style.backgroundColor="red"
+    }
+    else if (input==blue) {
+        document.body.style.backgroundColor = "blue";
+        input.style.backgroundColor="blue"
+    }
+
+    else if (input==green) {
+        document.body.style.backgroundColor = "green";
+        input.style.backgroundColor="green"
+    }
+
+    else if (input==yellow) {
+        document.body.style.backgroundColor = "yellow";
+        input.style.backgroundColor="yellow"
+    }
+
+    else if (input==orange) {
+        document.body.style.backgroundColor = "orange";
+        input.style.backgroundColor="orange"
+    }
+    else if(input==color){
+        console.log("inside",color);
+    }
+
+}
