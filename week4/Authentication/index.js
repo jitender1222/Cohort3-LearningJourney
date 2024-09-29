73626928db9c2310e16c067f816f9065beb2f38a
jitender1222 <jeetusinghkanyal@gmail.com>
2024-09-30 01:04:17 +0530

    added me endpoint

--- week4/Authentication/index.js
+++ week4/Authentication/index.js
@@ -6,7 +6,8 @@ app.use(express.json());
 const users=[];
 
     function generateToken() {
-        let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+        let options = 
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
         'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 
@@ -21,22 +22,26 @@ const users=[];
     }
 
 const signup=app.post("/signup",(req,res)=>{
-    const userName=req.body.userName;
+    const username=req.body.username;
     const password=req.body.password;
 
     users.push({
-        userName,
+        username,
         password
     })
 
     res.send({
         message:"You have signed up"
     })
+    console.log(users);
 });
 
 const signin=app.post("/signin",(req,res)=>{
-    const user=users.find(user => user.userName === userName  && user.password === password );
+    const username=req.body.username;
+    const password=req.body.password;
+    const user=users.find(user => user.username === username  && user.password === password );
     if(user){
+        console.log(user);
         const token=generateToken();
         user.token=token;
         res.send({
@@ -48,6 +53,24 @@ const signin=app.post("/signin",(req,res)=>{
             message:"Invalid username or password"
         })
     }
+    console.log(user);
+    console.log(users);
+})
+
+app.get("/me",(req,res)=>{
+    const token=req.headers.token;
+    const user=users.find(user => user.token == token );
+    if(user){
+        res.send({
+            username:user.username
+        })
+    }
+    else{
+        res.status(400).send({
+            message:"user not found"
+        })
+    }
+    console.log(token);
 })
 
 
