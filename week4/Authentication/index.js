9cd4b9cada77437200d8a63b7e478854c3d6a0d2
jitender1222 <jeetusinghkanyal@gmail.com>
2024-09-30 09:00:54 +0530

    replace token with JWT

--- week4/Authentication/index.js
+++ week4/Authentication/index.js
@@ -1,25 +1,12 @@
 const express=require("express");
+const jwt=require("jsonwebtoken");
 const app=express();
+const JWT_SECRET="heyimjitender"
 
 app.use(express.json());
 
 const users=[];
 
-    function generateToken() {
-        let options = 
-        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-        'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
-        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 
-        'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    
-        let token = "";
-        for (let i = 0; i < 32; i++) {
-            // use a simple function here
-            token += options[Math.floor(Math.random() * options.length)];
-        }
-        return token;
-    }
 
 const signup=app.post("/signup",(req,res)=>{
     const username=req.body.username;
@@ -41,9 +28,9 @@ const signin=app.post("/signin",(req,res)=>{
     const password=req.body.password;
     const user=users.find(user => user.username === username  && user.password === password );
     if(user){
-        console.log(user);
-        const token=generateToken();
-        user.token=token;
+        const token=jwt.sign({  // convert username into jsonwebtoken
+            username:username  // what we want to encrypt 
+        },JWT_SECRET);   
         res.send({
             token
         })
@@ -53,26 +40,24 @@ const signin=app.post("/signin",(req,res)=>{
             message:"Invalid username or password"
         })
     }
-    console.log(user);
-    console.log(users);
 })
 
 app.get("/me",(req,res)=>{
-    const token=req.headers.token;
-    const user=users.find(user => user.token == token );
+    const token=req.headers.token;  //jwt
+    const decodedToken=jwt.verify(token,JWT_SECRET); // decode the token 
+    const decodedUserName=decodedToken.username   // get the username
+    const user=users.find(user => user.username == decodedUserName );
     if(user){
         res.send({
-            username:user.username
+            username:user.username 
         })
     }
     else{
         res.status(400).send({
-            message:"user not found"
+            message:"token invalid"
         })
     }
-    console.log(token);
 })
-
 
 app.listen(3000,()=>{
     console.log("app is listen on port 3000");
