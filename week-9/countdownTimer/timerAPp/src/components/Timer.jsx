c3b7caf09ccfbc6701302b9be85e2fa09841bfe0
jitender1222 <jeetusinghkanyal@gmail.com>
2024-12-12 13:11:41 +0530

    added start/pause and reset functionality

--- week-9/countdownTimer/timerAPp/src/components/Timer.jsx
+++ week-9/countdownTimer/timerAPp/src/components/Timer.jsx
@@ -1,27 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Timer = () => {
-  let time = 0;
   const [start, setStart] = useState(false);
-  function onStart(time) {
+  const [time, setTime] = useState(0);
+  const [isEdit, setIsEdit] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+    if (start) {
+      console.log("inside start");
+      intervalId = setInterval(() => {
+        setTime(time + 1);
+      }, 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [start, time]);
+
+  const hours = Math.floor(time / 360000);
+
+  // Minutes calculation
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  // Seconds calculation
+  const seconds = Math.floor((time % 6000) / 100);
+
+  // Milliseconds calculation
+  const milliseconds = time % 100;
+
+  function onStart() {
     setStart(!start);
-    setInterval(time, 1000);
   }
+
   function onReset() {
-    console.log(onReset);
+    setTime(0);
   }
   return (
     <>
       <div className="outer-div">
         <div className="inner-div">
           <div className="circle">
-            <div className="timer">{time}</div>
+            <div className="timer">
+              {hours}:{minutes.toString().padStart(2, 0)}:
+              {seconds.toString().padStart(2, 0)}:
+              {milliseconds.toString().padStart(2, 0)}
+            </div>
           </div>
           <div className="btn-outer">
-            <button onClick={() => onStart(time)} className="btn btn-start">
+            <button onClick={() => onStart()} className="btn btn-start">
               {start ? "Pause" : "Start"}
             </button>
-            <button onClick={onReset} className="btn btn-reset">
+            <button onClick={() => onReset()} className="btn btn-reset">
               Reset
             </button>
           </div>
